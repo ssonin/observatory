d4f030b60ad5ff12e450d67f1e631eda154d12d4
Sergei Sonin <sonin.sm@gmail.com>
2021-04-14 15:52:32 +0300

    MS2 Add spatial interpolation

--- src/main/scala/observatory/Main.scala
+++ src/main/scala/observatory/Main.scala
@@ -1,9 +1,28 @@
 package observatory
 
+import scala.math._
+
 object Main extends App {
 
-  val list = Extraction.locateTemperatures(2015, "/stations.csv", "/2015.csv").take(100)
-  val avg = Extraction.locationYearlyAverageRecords(list)
-  println(avg)
+  val temperatures: Iterable[(Location, Temperature)] =
+    Extraction.locationYearlyAverageRecords(
+      Extraction.locateTemperatures(2015, "/stations.csv", "/2015.csv").take(100))
+
+  val df = Visualization.predictTemperature(temperatures, Location(32.95, 65.567))
+
+  println(df)
+
+//  val (lat1, lon1) = (64.533, 12.383)
+//  val (lat2, lon2) = (32.95, 65.567)
+//  val d = acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon2 - lon1)) * 6371
+
+//  val (lat1, lon1) = (56.153, 40.426)
+//  val (lat2, lon2) = (56.984, 40.967)
+//  val (lat1, lon1) = (64.533, 12.383)
+//  val (lat2, lon2) = (32.95, 65.567)
+//  val (lat1r, lon1r, lat2r, lon2r) = (lat1 * Pi / 180, lon1 * Pi / 180, lat2 * Pi / 180, lon2 * Pi / 180)
+//  val d = acos(sin(lat1r) * sin(lat2r) + cos(lat1r) * cos(lat2r) * cos(abs(lon1r - lon2r))) * 6371
+//
+//  println(d)
 
 }
--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -1,11 +1,22 @@
 package observatory
 
+import observatory.Config.{sc, spark}
 import com.sksamuel.scrimage.{Image, Pixel}
+import org.apache.spark.sql.{Column, DataFrame}
+import org.apache.spark.sql.functions._
+
+import scala.math.Pi
 
 /**
   * 2nd milestone: basic visualization
   */
 object Visualization extends VisualizationInterface {
+
+  import spark.implicits._
+
+  private val iwdPower = 2
+  private val earthRadius = 6371
+  private val distancePrecision = 1
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
@@ -13,7 +24,47 @@ object Visualization extends VisualizationInterface {
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
-    ???
+    val df = temperatures.map {case (loc, temp) => (loc.lat, loc.lon, temp)}
+      .toSeq.toDF("lat_i", "lon_i", "temp")
+      .withColumn("lat", lit(location.lat))
+      .withColumn("lon", lit(location.lon))
+      .withColumn("distance", distance($"lat_i", $"lon_i", $"lat", $"lon"))
+
+    val close = df.where($"distance" === 0)
+    if (close.isEmpty) sparkPredict(df) else df.first().getAs[Temperature]("temperature")
+  }
+
+//  def sparkPredictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
+//    val df = temperatures.map {case (loc, temp) => (loc.lat, loc.lon, temp)}
+//      .toSeq.toDF("lat_i", "lon_i", "temp")
+//      .withColumn("lat", lit(location.lat))
+//      .withColumn("lon", lit(location.lon))
+//      .withColumn("distance", distance($"lat_i", $"lon_i", $"lat", $"lon"))
+//
+//    val close = df.where($"distance" === 0)
+//    if (close.isEmpty) sparkPredict(df) else df.first().getAs[Temperature]("temperature")
+//  }
+
+  def sparkPredict(temperatures: DataFrame): Temperature = {
+    val augmented = temperatures.withColumn("denomTerm", lit(1) / pow(temperatures("distance"), 2))
+    val nomTerm: Column = (augmented("temp") * augmented("denomTerm")).as("nomTerm")
+    val r = augmented
+      .select(nomTerm, $"denomTerm")
+      .agg(sum($"nomTerm").as("nom"), sum($"denomTerm").as("denom"))
+      .select($"nom", $"denom")
+      .first()
+
+    r.getAs[Double]("nom") / r.getAs[Double]("denom")
+  }
+
+  def distance(lat1: Column, lon1: Column, lat2: Column, lon2: Column): Column = {
+    val calculated = when(lat1 === lat2 && lon1 === lon2, 0)
+      .when(lat1 === negate(lat2) && (lon1 === (lon2 + Pi) || lon1 === (lon2 - Pi)), Pi)
+      .otherwise {
+        val (lat1r, lon1r, lat2r, lon2r) = (radians(lat1), radians(lon1), radians(lat2), radians(lon2))
+        acos(sin(lat1r) * sin(lat2r) + cos(lat1r) * cos(lat2r) * cos(abs(lon1r - lon2r)))
+      } * earthRadius
+    when(calculated <= distancePrecision, 0).otherwise(calculated)
   }
 
   /**
