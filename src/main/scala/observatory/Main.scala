788d9f45db075c42ce313ed6324ec716fefa605f
Sergei Sonin <sonin.sm@gmail.com>
2021-04-15 23:50:05 +0300

    MS2 Add linear interpolation

--- src/main/scala/observatory/Main.scala
+++ src/main/scala/observatory/Main.scala
@@ -4,25 +4,28 @@ import scala.math._
 
 object Main extends App {
 
-  val temperatures: Iterable[(Location, Temperature)] =
-    Extraction.locationYearlyAverageRecords(
-      Extraction.locateTemperatures(2015, "/stations.csv", "/2015.csv").take(100))
-
-  val df = Visualization.predictTemperature(temperatures, Location(32.95, 65.567))
-
-  println(df)
-
-//  val (lat1, lon1) = (64.533, 12.383)
-//  val (lat2, lon2) = (32.95, 65.567)
-//  val d = acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon2 - lon1)) * 6371
-
-//  val (lat1, lon1) = (56.153, 40.426)
-//  val (lat2, lon2) = (56.984, 40.967)
-//  val (lat1, lon1) = (64.533, 12.383)
-//  val (lat2, lon2) = (32.95, 65.567)
-//  val (lat1r, lon1r, lat2r, lon2r) = (lat1 * Pi / 180, lon1 * Pi / 180, lat2 * Pi / 180, lon2 * Pi / 180)
-//  val d = acos(sin(lat1r) * sin(lat2r) + cos(lat1r) * cos(lat2r) * cos(abs(lon1r - lon2r))) * 6371
+//  val temperatures: Iterable[(Location, Temperature)] =
+//    Extraction.locationYearlyAverageRecords(
+//      Extraction.locateTemperatures(2015, "/stations.csv", "/2015.csv").take(100))
 //
-//  println(d)
+//  val df = Visualization.predictTemperature(temperatures, Location(32.95, 65.567))
+//  println(df)
 
+//  Color(128,0,128) (scale = List((0.0,Color(255,0,0)), (1.0,Color(0,0,255))), value = 0.5)
+
+//  val scale = List((0.0,Color(255,0,0)), (1.0,Color(0,0,255)))
+//  val temperature = 0.5
+//  val scale = List((1.0,Color(255,0,0)), (2.0,Color(0,0,255)), (3.0,Color(0,255,0)))
+//  val temperature = 1.5
+
+//  val scale = List((1.0,Color(255,0,0)), (2.0,Color(0,0,255)), (3.0,Color(0,255,0)))
+//  val temperature = 1.25
+
+  val scale = List((1.0,Color(255,0,0)), (2.0,Color(0,0,255)), (3.0,Color(0,255,0)))
+  val temperature = 2.5
+
+  val color = Visualization.interpolateColor(scale, temperature)
+  //List((1.0,Color(255,0,0)), (2.0,Color(0,0,255)), (3.0,Color(0,255,0)))
+
+  println(color)
 }
--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -1,18 +1,14 @@
 package observatory
 
-import observatory.Config.{sc, spark}
 import com.sksamuel.scrimage.{Image, Pixel}
-import org.apache.spark.sql.{Column, DataFrame}
-import org.apache.spark.sql.functions._
 
-import scala.math.Pi
+import scala.annotation.tailrec
+import scala.math.{abs, acos, cos, pow, round, sin, toRadians}
 
 /**
   * 2nd milestone: basic visualization
   */
 object Visualization extends VisualizationInterface {
-
-  import spark.implicits._
 
   private val iwdPower = 2
   private val earthRadius = 6371
@@ -20,52 +16,37 @@ object Visualization extends VisualizationInterface {
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
-    * @param location Location where to predict the temperature
+    * @param target Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
-  def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
-    val df = temperatures.map {case (loc, temp) => (loc.lat, loc.lon, temp)}
-      .toSeq.toDF("lat_i", "lon_i", "temp")
-      .withColumn("lat", lit(location.lat))
-      .withColumn("lon", lit(location.lon))
-      .withColumn("distance", distance($"lat_i", $"lon_i", $"lat", $"lon"))
-
-    val close = df.where($"distance" === 0)
-    if (close.isEmpty) sparkPredict(df) else df.first().getAs[Temperature]("temperature")
+  def predictTemperature(temperatures: Iterable[(Location, Temperature)], target: Location): Temperature = {
+    val parTemperatures = temperatures.par
+    val equalToTarget = parTemperatures.filter{case (loc, _) => loc == target}
+    if (equalToTarget.isEmpty) {
+      val distances = parTemperatures.map{case(loc, temp) => (greatCircleDistance(loc, target), temp)}
+      val closeOnes = distances.filter{case (distance, _) => distance < distancePrecision}
+      if (closeOnes.isEmpty) {
+        val (nom, denom) =
+          distances.map{case (distance, temp) => idwTerm(distance, temp)}
+            .aggregate((0.0, 0.0))(
+              {case ((nom, denom), term) => (nom + term._1, denom + term._2)},
+              {case ((nomL, denomL), (nomR, denomR)) => (nomL + nomR, denomL + denomR)})
+        nom / denom
+      } else closeOnes.head._2
+    } else equalToTarget.head._2
   }
 
-//  def sparkPredictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
-//    val df = temperatures.map {case (loc, temp) => (loc.lat, loc.lon, temp)}
-//      .toSeq.toDF("lat_i", "lon_i", "temp")
-//      .withColumn("lat", lit(location.lat))
-//      .withColumn("lon", lit(location.lon))
-//      .withColumn("distance", distance($"lat_i", $"lon_i", $"lat", $"lon"))
-//
-//    val close = df.where($"distance" === 0)
-//    if (close.isEmpty) sparkPredict(df) else df.first().getAs[Temperature]("temperature")
-//  }
-
-  def sparkPredict(temperatures: DataFrame): Temperature = {
-    val augmented = temperatures.withColumn("denomTerm", lit(1) / pow(temperatures("distance"), 2))
-    val nomTerm: Column = (augmented("temp") * augmented("denomTerm")).as("nomTerm")
-    val r = augmented
-      .select(nomTerm, $"denomTerm")
-      .agg(sum($"nomTerm").as("nom"), sum($"denomTerm").as("denom"))
-      .select($"nom", $"denom")
-      .first()
-
-    r.getAs[Double]("nom") / r.getAs[Double]("denom")
+  def idwTerm(distance: Double, temp: Temperature): (Double, Double) = {
+    val coeff = pow(distance, iwdPower)
+    (temp / coeff, 1 / coeff)
   }
 
-  def distance(lat1: Column, lon1: Column, lat2: Column, lon2: Column): Column = {
-    val calculated = when(lat1 === lat2 && lon1 === lon2, 0)
-      .when(lat1 === negate(lat2) && (lon1 === (lon2 + Pi) || lon1 === (lon2 - Pi)), Pi)
-      .otherwise {
-        val (lat1r, lon1r, lat2r, lon2r) = (radians(lat1), radians(lon1), radians(lat2), radians(lon2))
-        acos(sin(lat1r) * sin(lat2r) + cos(lat1r) * cos(lat2r) * cos(abs(lon1r - lon2r)))
-      } * earthRadius
-    when(calculated <= distancePrecision, 0).otherwise(calculated)
+  def greatCircleDistance(from: Location, to: Location): Double = {
+    val (fromR, toR) = (radians(from), radians(to))
+    acos(sin(fromR.lat) * sin(toR.lat) + cos(fromR.lat) * cos(toR.lat) * cos(abs(fromR.lon - toR.lon))) * earthRadius
   }
+
+  def radians(loc: Location): Location = Location(toRadians(loc.lat), toRadians(loc.lon))
 
   /**
     * @param points Pairs containing a value and its associated color
@@ -73,7 +54,31 @@ object Visualization extends VisualizationInterface {
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Temperature, Color)], value: Temperature): Color = {
-    ???
+    require(points.nonEmpty, "Color scale cannot be empty")
+    type ScaleVal = (Temperature, Color)
+
+    @tailrec
+    def walk(scale: Seq[ScaleVal], prev: (Temperature, Color)): (ScaleVal, ScaleVal) = {
+      if (scale.isEmpty) (prev, prev)
+      else if (scale.head._1 < value) walk(scale.tail, scale.head)
+      else (prev, scale.head)
+    }
+
+    def interpolate(x0: Temperature, y0: Int, x1: Temperature, y1: Int, x: Temperature): Int = {
+      round(y0 + (x - x0) * (y1 - y0) / (x1 - x0)).toInt
+    }
+
+    val sorted = points.toIndexedSeq.sortBy {case (temperature, _) => temperature}
+    if (value <= sorted.head._1) sorted.head._2
+    else if (value >= sorted.last._1) sorted.last._2
+    else {
+      val ((x0, y0), (x1, y1)) = walk(sorted, (value, Color(0, 0, 0)))
+      Color(
+        interpolate(x0, y0.red, x1, y1.red, value),
+        interpolate(x0, y0.green, x1, y1.green, value),
+        interpolate(x0, y0.blue, x1, y1.blue, value)
+      )
+    }
   }
 
   /**
