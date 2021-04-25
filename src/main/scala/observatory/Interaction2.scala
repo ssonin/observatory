acd36caa99b1ae4310ef739cee164b3fdecbe2c8
Sergei Sonin <sonin.sm@gmail.com>
2021-04-25 21:26:32 +0300

    MS6 Add interactive user interface

--- src/main/scala/observatory/Interaction2.scala
+++ src/main/scala/observatory/Interaction2.scala
@@ -1,5 +1,7 @@
 package observatory
 
+import observatory.LayerName.{Deviations, Temperatures}
+
 /**
   * 6th (and last) milestone: user interface polishing
   */
@@ -8,52 +10,52 @@ object Interaction2 extends Interaction2Interface {
   /**
     * @return The available layers of the application
     */
-  def availableLayers: Seq[Layer] = {
-    ???
-  }
+  def availableLayers: Seq[Layer] = List(
+    Layer(Temperatures, temperaturesColorScale, 1975 to 2015),
+    Layer(Deviations, deviationsColorScale, 1991 to 2015))
 
   /**
     * @param selectedLayer A signal carrying the layer selected by the user
     * @return A signal containing the year bounds corresponding to the selected layer
     */
-  def yearBounds(selectedLayer: Signal[Layer]): Signal[Range] = {
-    ???
-  }
+  def yearBounds(selectedLayer: Signal[Layer]): Signal[Range] = Signal(selectedLayer().bounds)
 
   /**
     * @param selectedLayer The selected layer
-    * @param sliderValue The value of the year slider
+    * @param sliderValue   The value of the year slider
     * @return The value of the selected year, so that it never goes out of the layer bounds.
     *         If the value of `sliderValue` is out of the `selectedLayer` bounds,
     *         this method should return the closest value that is included
     *         in the `selectedLayer` bounds.
     */
   def yearSelection(selectedLayer: Signal[Layer], sliderValue: Signal[Year]): Signal[Year] = {
-    ???
+    Signal({
+      val bounds = yearBounds(selectedLayer)()
+      val year = sliderValue()
+      if (bounds.min >= year) bounds.min
+      else if (bounds.max <= year) bounds.max
+      else sliderValue()
+    })
   }
 
   /**
     * @param selectedLayer The selected layer
-    * @param selectedYear The selected year
+    * @param selectedYear  The selected year
     * @return The URL pattern to retrieve tiles
     */
-  def layerUrlPattern(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = {
-    ???
-  }
+  def layerUrlPattern(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] =
+    Signal(s"target/${selectedLayer().layerName.id}/${selectedYear()}/{zoom}/{x}-{y}.png")
 
   /**
     * @param selectedLayer The selected layer
-    * @param selectedYear The selected year
+    * @param selectedYear  The selected year
     * @return The caption to show
     */
-  def caption(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = {
-    ???
-  }
+  def caption(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] =
+    Signal(s"${selectedLayer().layerName.id.capitalize} (${selectedYear()})")
 
 }
 
-// Interface used by the grading infrastructure. Do not change signatures
-// or your submission will fail with a NoSuchMethodError.
 trait Interaction2Interface {
   def availableLayers: Seq[Layer]
   def yearBounds(selectedLayer: Signal[Layer]): Signal[Range]
--- src/main/scala/observatory/package.scala
+++ src/main/scala/observatory/package.scala
@@ -4,4 +4,22 @@ package object observatory {
 
   val (tileWidth, tileHeight) = (256, 256)
   val alpha = 127
+
+  val temperaturesColorScale = List(
+    (60.0, Color(255, 255, 255)),
+    (32.0, Color(255, 0, 0)),
+    (12.0, Color(255, 255, 0)),
+    (0.0, Color(0, 255, 255)),
+    (-15.0, Color(0, 0, 255)),
+    (-27.0, Color(255, 0, 255)),
+    (-50.0, Color(33, 0, 107)),
+    (-60.0, Color(0, 0, 0)))
+
+  val deviationsColorScale = List(
+    (7.0, Color(0, 0, 0)),
+    (4.0, Color(255, 0, 0)),
+    (2.0, Color(255, 255, 0)),
+    (0.0, Color(255, 255, 255)),
+    (-2.0, Color(0, 255, 255)),
+    (-7.0, Color(0, 0, 255)))
 }
